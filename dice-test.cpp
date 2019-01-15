6449b6c96967ee062cbb85df3d5fef0f5a00dd89
Vaughan, Thomas E <tevaugha@ball.com>
2019-01-15 15:28:57 -0700

    Refactored test code for reasonable functions.

--- dice-test.cpp
+++ dice-test.cpp
@@ -54,30 +54,34 @@ double var(double nk, double n) { return nk * (1.0 - nk / n); }
 /// @return    Estimate standard deviation for case of equal weights.
 double stdev(double nk, double n) { return sqrt(var(nk, n)); }
 
-TEST_CASE("Bell curve is produced for 3d6.", "[dice]") {
-  enum { N = 1000, D = 3, F = 6, MAX = D * F, MIN = D, SZ = MAX - MIN + 1 };
-  double constexpr NSD = 4.0;
-  vector<unsigned>    h(SZ, 0);
-  roll_sequence const r(N, D, F);
-  for (auto e : r) {
-    ++h[e - D]; // Add element to histogram.
-  }
-  double min = N, max = 0;
-  for (unsigned i = 0; i < SZ / 2; ++i) {
-    // Test symmetry of distribution.
-    unsigned const j  = SZ - i - 1;
-    double const   nl = h[i];
-    double const   nr = h[j];
-    double const   vl = var(nl, N);
-    double const   vr = var(nr, N);
-    REQUIRE(fabs(nl - nr) <= NSD * sqrt(vl + vr));
-    if (i > 0) {
-      // Test monotonicity of each wing.
-      double const nll = h[i - 1];
-      double const nrr = h[j + 1];
-      REQUIRE(nl >= nll - NSD * sqrt(vl + var(nll, N)));
-      REQUIRE(nr >= nrr - NSD * sqrt(vr + var(nrr, N)));
-    }
+/// Number of standard deviations of tolerance for bell-curve test.
+double constexpr NSD = 4.0;
+
+/// Total number of counts for histogram in bell-curve test.
+unsigned constexpr N = 1000;
+
+/// Number of dice used in each roll in bell-curve test.
+unsigned constexpr D = 3;
+
+/// Minimum and maximum values for coordinates in counts-space in
+/// histogram-plot of bell-curve.
+struct hlim {
+  double min; ///< Minimum coordinate (counts).
+  double max; ///< Maximum coordinate (counts).
+
+  /// Initialize values so that first call to update() is guaranteed to change
+  /// both limits.
+  hlim() : min(N), max(0) {}
+
+  /// Update min and max.
+  ///
+  /// Passed in is a pair of counts, each corresponding to one of the two cells
+  /// at the same distance from what should be the center of symmetry for the
+  /// distribution in the histogram.
+  ///
+  /// @param nl   Number of counts at cell left of center.
+  /// @param nr   Number of counts at cell left of center.
+  void update(double nl, double nr) {
     double const sl = NSD * stdev(nl, N);
     double const sr = NSD * stdev(nr, N);
     if (nl + sl > max) {
@@ -93,19 +97,25 @@ TEST_CASE("Bell curve is produced for 3d6.", "[dice]") {
       min = nr - sr;
     }
   }
+};
+
+/// Draw histogram that is used in bell-curve test.
+/// @param sz   Number of cells in histogram.
+/// @param lim  Limits of drawing in counts-space.
+/// @param h    Data for histogram.
+void draw_histogram(unsigned sz, hlim const &lim, vector<unsigned> const &h) {
   printf("\n");
-  for (unsigned i = 0; i < SZ; ++i) {
-    unsigned const k = i + 3;
+  for (unsigned i = 0; i < sz; ++i) {
+    unsigned const k = i + D;
     printf("%02d ", k);
     double constexpr MAX_W = 69.99; // Histogram width in characters.
-    double const   c       = MAX_W / (max - min);
+    double const   c       = MAX_W / (lim.max - lim.min);
     double const   s       = c * NSD * stdev(h[i], N);
-    double const   m       = c * (h[i] - min);
-    double const   z       = c * (0 - min);
+    double const   m       = c * (h[i] - lim.min);
     unsigned const im      = unsigned(m);
     unsigned const il      = unsigned(m - s);
     unsigned const ih      = unsigned(m + s);
-    unsigned const iz      = unsigned(z);
+    unsigned const iz      = unsigned(c * (0 - lim.min));
     for (unsigned j = 0; j <= ih; ++j) {
       if (j == im) {
         printf("#");
@@ -122,4 +132,32 @@ TEST_CASE("Bell curve is produced for 3d6.", "[dice]") {
     printf("\n");
   }
   printf("\n");
+}
+
+TEST_CASE("Bell curve is produced for 3d6.", "[dice]") {
+  enum { F = 6, MAX = D * F, MIN = D, SZ = MAX - MIN + 1 };
+  vector<unsigned>    h(SZ, 0);
+  roll_sequence const r(N, D, F);
+  for (auto e : r) {
+    ++h[e - D]; // Add element to histogram.
+  }
+  hlim limits;
+  for (unsigned i = 0; i < SZ / 2; ++i) {
+    // Test symmetry of distribution.
+    unsigned const j  = SZ - i - 1;
+    double const   nl = h[i];
+    double const   nr = h[j];
+    double const   vl = var(nl, N);
+    double const   vr = var(nr, N);
+    REQUIRE(fabs(nl - nr) <= NSD * sqrt(vl + vr));
+    if (i > 0) {
+      // Test monotonicity of each wing.
+      double const nll = h[i - 1];
+      double const nrr = h[j + 1];
+      REQUIRE(nl >= nll - NSD * sqrt(vl + var(nll, N)));
+      REQUIRE(nr >= nrr - NSD * sqrt(vr + var(nrr, N)));
+    }
+    limits.update(nl, nr);
+  }
+  draw_histogram(SZ, limits, h);
 }
